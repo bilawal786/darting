c4c66a4cf774cd3f46f6443db0809fde39ff65ba
mohamedbilawal <bilawal.jabbar.786@gmail.com>
2022-06-16 09:25:44 +0500

    new

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'Front\FrontendController@index')->name('front.index');
 Route::get('/wizard/form', 'Front\FrontendController@wizard')->name('front.wizard');
 Route::get('/front/function', 'Front\FrontendController@function')->name('front.function');
+Route::post('/find/match', 'Front\FrontendController@findMatch')->name('find.match');
 Route::post('/front/profiles', 'Front\FrontendController@profiles')->name('front.profiles');
 
 
@@ -25,6 +26,5 @@ Auth::routes();
 Route::group(['middleware' => ['auth', 'role']], function() {
 
 Route::get('/home', 'HomeController@index')->name('home');
-    Route::post('/find/match', 'Front\FrontendController@findMatch')->name('find.match');
 
 });
