f6b81afcdd57ef1758909b5d50f3b83bcabb82fb
usman299 <usman@codingcrust.com>
2022-07-06 21:11:16 -0700

    Question

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Cashier\Billable;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -49,6 +50,7 @@ class User extends Authenticatable
     }
     public function hasSubscription(){
         $user = Auth::user();
+       
         if ($user->subscribed('default')){
             return true;
         }else{
