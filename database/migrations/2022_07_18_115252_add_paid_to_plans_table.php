e76ee9c3aaee8fe333ab1d54c60f050a08d87070
Sonu <bilawal.jabbar.786@gmail.com>
2022-07-19 09:30:31 +0500

    Test

--- database/migrations/2022_07_18_115252_add_paid_to_plans_table.php
+++ database/migrations/2022_07_18_115252_add_paid_to_plans_table.php
@@ -14,7 +14,7 @@ class AddPaidToPlansTable extends Migration
     public function up()
     {
         Schema::table('plans', function (Blueprint $table) {
-            $table->string('user_id')->default('monthly')->after('description');
+            $table->string('duration')->default('monthly')->after('description');
         });
     }
 
@@ -26,7 +26,7 @@ class AddPaidToPlansTable extends Migration
     public function down()
     {
         Schema::table('plans', function (Blueprint $table) {
-            $table->string('user_id')->default('monthly')->after('description');
+            $table->string('duration')->default('monthly')->after('description');
         });
     }
 
